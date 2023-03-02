d2eb24bd3a6ae7582d24ff5bbddf85eeef31b005
Michael Jurek <michaeljurek@seznam.cz>
2023-03-02 15:35:32 +0100

    Apache install for versions 2.4.26 to 2.4.55

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # apache2_scripts
 Bash scripts to configure Apache2 web server
+
+## [apache_install.sh](apache_install.sh)
+Apache Install from source code
+> works for versions 2.4.26 - 2.4.55
--- apache_install.sh
+++ apache_install.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
+# Define Apache version to be installed (default is Apache 2.4.54)
+APACHE_VERSION=${1:-2.4.54}
+
 # Define the installation directory for Apache
-#INSTALL_DIR="/usr/local/apache2"
-INSTALL_DIR="./tmp/apache2"
+INSTALL_DIR="/usr/local/apache$APACHE_VERSION"
+
 
 # Define the paths to the required libraries
-#OPENSSL_PATH="/usr/local/openssl"
-#PCRE_PATH="/usr/local/pcre"
-OPENSSL_PATH="./tmp/openssl"
-PCRE_PATH="./tmp/pcre"
+OPENSSL_PATH="/usr/local/openssl"
+PCRE_PATH="/usr/local/pcre"
 
-# Define Apache version to be installed
-APACHE_VERSION=${1:-2.4.54}
+print_usage(){
+    echo "Usage: apache_install.sh [VERSION] "
+    echo "Default VERSION is 2.4.54"
+}
 
 # Install necessary tools and libraries
 sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev libpcre3-dev libapr1-dev libaprutil1-dev
 
-echo "TEST: $APACHE_VERSION"
-
 ##=======================
-# 1) Download Apache source code from the Apache website
-#wget "http://archive.apache.org/dist/httpd/httpd-2.4.38.tar.gz"
+# 1) Download Apache source code and install it
+wget "http://archive.apache.org/dist/httpd/httpd-$APACHE_VERSION.tar.gz"
+# extract source code
+tar -xf httpd-$APACHE_VERSION.tar.gz
+cd httpd-$APACHE_VERSION/
+# configure Apache with desired options
+# --prefix --> specifies installation directory
+# --enable-mods-shared --> enables all modules to be shared
+# ssl --> for HTTPS
+# pcre -->
+./configure --prefix=${INSTALL_DIR} --enable-mods-shared=all #--enable-ssl --with-ssl=${OPENSSL_PATH} --with-pcre=${PCRE_PATH}
+# compile source code
+make
+# install Apache
+sudo make install
+cd ../
+
+# 2) 
+
+# Verify instalation
+${INSTALL_DIR}/bin/apachectl -t
